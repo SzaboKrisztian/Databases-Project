ff2a6e9b02b6efa0f9a105705fbcd40caf9f9719
Radu Onescu <radu@html24.net>
2021-03-09 14:43:16 +0100

    Added stored objects

--- mysql/db_create_scripts.sql
+++ mysql/db_create_scripts.sql
@@ -35,7 +35,7 @@ CREATE TABLE `user` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(255) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
-  `role` ENUM('user', 'manager', 'admin') NOT NULL,
+  `role` ENUM('user', 'manager', 'admin') NOT NULL DEFAULT('user'),
   `verified` TINYINT NOT NULL DEFAULT(0),
   `personal_data_id` INT,
   `deleted` TINYINT NOT NULL DEFAULT(0),
@@ -211,7 +211,7 @@ CREATE TABLE `order` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `user_id` INT NOT NULL,
   `seller_id` INT NOT NULL,
-  `order_status` ENUM('pending','confirmed','sent','delivered','cancelled') NOT NULL,
+  `order_status` ENUM('pending','confirmed','sent','delivered','cancelled') NOT NULL DEFAULT('pending'),
   `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   `updated_at` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
@@ -249,3 +249,132 @@ CREATE TABLE `audit` (
 
 ALTER TABLE `audit`
 ADD FOREIGN KEY (`user_id`) REFERENCES `user`(`id`);
+
+-- VIEW ALL USERS
+DROP VIEW IF EXISTS user_data;
+CREATE VIEW user_data AS
+    SELECT 
+        user.id,
+        user.email,
+        user.role,
+        user.verified,
+        user.created_at,
+        personal_data.first_name,
+        personal_data.last_name,
+        personal_data.phone_number,
+        personal_data.date_of_birth,
+        address.text,
+        address.street,
+        address.number,
+        address.floor,
+        address.door,
+        address.zip_code,
+        address.city
+    FROM user
+	JOIN personal_data ON user.personal_data_id = personal_data.id
+	JOIN address ON personal_data.address_id = address.id
+    WHERE user.deleted = 0;
+-- SELECT * FROM user_data;
+
+-- VIEW A PRODUCT
+DROP PROCEDURE IF EXISTS view_product;
+DELIMITER \\
+CREATE PROCEDURE view_product(
+	in in_id int
+)
+BEGIN
+	SELECT 
+		p.id, p.name, p.code, pd.description, m.name as manufacturer, c.name as category
+    FROM product p
+    JOIN product_description pd on p.description_id = pd.id
+    JOIN manufacturer m on p.manufacturer_id = m.id
+    JOIN category c on p.category_id = c.id
+    WHERE p.id = in_id AND p.deleted = 0 AND p.approved = 1;
+END \\
+
+DELIMITER ;
+-- call view_product(1);
+
+-- GET NUMBER OF PRODUCTS IN A CATEGORY
+DROP FUNCTION IF EXISTS get_product_count_for_category;
+DELIMITER \\
+CREATE FUNCTION get_product_count_for_category(
+	in_id int
+)
+RETURNS int
+READS SQL DATA
+BEGIN
+	DECLARE count_no INT;
+	SELECT COUNT(*) into count_no from product WHERE category_id = in_id;
+    RETURN count_no;
+END \\
+
+DELIMITER ;
+-- SELECT get_product_count_for_category(1);
+
+-- VIEW ALL USERS
+DROP VIEW IF EXISTS seller_product;
+CREATE VIEW seller_product AS
+SELECT 
+	sellers_products.id, sellers_products.original_price, sellers_products.sale_price, sellers_products.stock_qty, seller.name, seller.legal_name, seller.cvr, seller.phone_number, product.name as product_name, product.code as product_code
+FROM sellers_products
+JOIN seller on sellers_products.seller_id = seller.id
+JOIN product on sellers_products.product_id = product.id
+WHERE sellers_products.deleted = 0;
+
+-- CREATE NEW USER
+DROP PROCEDURE IF EXISTS create_user;
+DELIMITER //
+
+CREATE PROCEDURE create_user(
+	IN in_email VARCHAR(255), 
+	IN in_password VARCHAR(255), 
+	IN in_first_name VARCHAR(255), 
+	IN in_last_name VARCHAR(255), 
+	IN in_date_of_birth TIMESTAMP )
+BEGIN
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+		BEGIN
+			ROLLBACK;
+			RESIGNAL;
+		END;
+	START TRANSACTION;
+    INSERT INTO personal_data(first_name, last_name, date_of_birth) VALUES (in_first_name, in_last_name, in_date_of_birth);
+    INSERT INTO user(email, password, personal_data_id) VALUES (in_email, in_password, (SELECT LAST_INSERT_ID()));
+	COMMIT;
+END //
+DELIMITER ;
+
+-- CALL create_user('radu@radu.com', 'dsadsadsadasda', 'radu', 'radu2', '1999-04-03 00:00:00');
+
+-- GET PRODUCT IMAGES
+DROP PROCEDURE IF EXISTS get_product_images;
+DELIMITER //
+CREATE PROCEDURE get_product_images(
+    IN product_id INT
+)
+BEGIN
+    SELECT i.id, i.url, i.created_at
+    FROM product_image i
+    WHERE i.product_id = product_id;
+END //
+DELIMITER ;
+
+-- CALL get_product_images(1);
+
+-- GET PRODUCT PROPERTIES
+DROP PROCEDURE IF EXISTS get_product_properties;
+DELIMITER //
+CREATE PROCEDURE get_product_properties(
+    IN product_id INT
+)
+BEGIN
+    SELECT pp.property_id, p.name, pp.value, p.unit
+    FROM products_properties pp
+    JOIN property p on pp.property_id = p.id
+    WHERE pp.product_id = product_id;
+END //
+
+DELIMITER ;
+
+-- CALL get_product_properties(1)
